276ae6da7753d59c9e3dbba509dfe31924941af0
VanshOPAlt <catuop66@gmail.com>
2025-04-09 08:45:57 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ fi
 
 echo "Pterodactyl Nodes Overloaded Fixed with one script"
 
-curl -L -o /var/lib/pterodactyl/volumes/script.sh https://raw.githubusercontent.com/VanshOPAlt/ hibernate-wings/main/src/script.sh
+curl -L -o /var/lib/pterodactyl/volumes/run.sh https://raw.githubusercontent.com/VanshOPAlt/ hibernate-wings/main/src/run.sh
 curl -L -o /var/lib/pterodactyl/volumes/HibernateX.jar https://raw.githubusercontent.com/VanshOPAlt/ hibernate-wings/main/src/HibernateX.jar
 sudo chmod -R 777 /var/lib/pterodactyl/volumes
-nohup /var/lib/pterodactyl/volumes/script.sh > /dev/null 2>&1 & 
+nohup /var/lib/pterodactyl/volumes/run.sh > /dev/null 2>&1 & 
 
 echo "Hibernation for Pterodactyl wings has been installed."
